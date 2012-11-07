fca123bddd2392bdded57d7db6b37ebe94a37a5a
rho <rho.rhoit@gmail.com>
2012-11-07 13:29:42 +0545

    Adding gitconfig and ssh settings

---
+++ git/main.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global core.editor "emacs -nw"
+git config --global color.ui true
+#git config --global user.name "Firstname Lastname"
+#git config --global user.email "your_email@youremail.com"
