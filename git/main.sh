c0a81b1328f0a512dad1777a829af329cacf7fe8
rho <rho.rhoit@gmail.com>
2012-12-25 10:29:48 +0545

    config git ask Q

--- git/main.sh
+++ git/main.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+if [ -e ~/.gitconfig ]; then
+    read -p "Username: " uname
+    read -p "E-mail: " email
+    git config --global user.name "$uname"
+    git config --global user.email "$email"
+fi
+
 git config --global core.editor "nano"
 git config --global color.ui true
 git config --global push.default simple # changed with git 2.0 setting simple to hide pop-up warnings
-#git config --global user.name "Firstname Lastname"
-#git config --global user.email "your_email@youremail.com"
-
 
 cp /usr/share/git/git-prompt.sh ~/.bashrc.d/git-prompt
 chmod +x ~/.bashrc.d/git-prompt
