5fe337f1d57b0f3b68434ee4c71bbdcae21eb109
rho <rho.rhoit@gmail.com>
2013-05-30 14:03:05 +0545

    updates
    * emacs
    * shell
    * added some new plugins

--- gnome/defapps.sh
+++ gnome/defapps.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cp ~/.local/share/applications/mimeapps.list .
-
-
+#cp ~/.local/share/applications/mimeapps.list .
+xdg-mime default evince.desktop application/pdf
--- synaptics/main.sh
+++ synaptics/main.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+#sleep 20
+
 synclient TapButton2=2
 synclient TapButton3=3
