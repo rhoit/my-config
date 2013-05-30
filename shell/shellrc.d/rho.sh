953487607b5f51f8408acf794c6a937e64d6b906
rho <rho.rhoit@gmail.com>
2013-05-30 14:03:05 +0545

    shell - shellrc.d/rho.sh wacom

---
+++ shell/shellrc.d/rho.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#
+# My personal preference
+#
+
+
+# * HELPERS
+# bundle up commands for operation
+
+# ** wacom
+alias wacom-touch-off='xsetwacom set "Wacom Bamboo 16FG 4x5 Finger pad" touch off'
