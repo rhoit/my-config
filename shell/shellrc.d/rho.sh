4d959495cfddaa4931aff777a67ae97ed513a561
rho <rho.rhoit@gmail.com>
2014-08-05 21:12:15 +0545

    shell - shellrc.d/rho.sh nemo

--- shell/shellrc.d/rho.sh
+++ shell/shellrc.d/rho.sh
@@ -11,10 +11,23 @@ alias woman="man --apropos $1"
 alias py="python"
 
 
-# gdb
-alias gdb="gdb -q"
-
-
 # WINE is the standard env variable
 export WIN="~/.wine/dosdevices/c:"
 alias c:="cd $WIN"
+
+
+# * WRAPPERS
+# extend application options
+
+alias gdb="gdb -q"
+
+
+function nemo {
+    ##
+    ### nemo (file browser) wrapper
+    if [[ $# -eq 0 ]]; then
+        setsid /usr/bin/nemo . # "nemo" is function, will cause recursion
+    else
+        setsid /usr/bin/nemo $@
+    fi
+}
