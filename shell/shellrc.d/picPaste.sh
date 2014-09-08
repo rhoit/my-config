9d7beab10208259f2cc13ea9b5c02a58b0ece49a
rho <rho.rhoit@gmail.com>
2014-09-08 21:11:14 +0545

    shell ~ rc.d/picPaste.sh

---
+++ shell/shellrc.d/picPaste.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+function picpaste {
+    link='http://www.picpaste.com/upload.php'
+    opts=( -F storetime=1 -F addprivacy=1 -F rules=yes )
+    curl -sA firefox "${opts[@]}" -F upload=@"$1" "$link" | sed -n '/Picture URL/{n;s/.*">//;s/<.*//p}'
+}
