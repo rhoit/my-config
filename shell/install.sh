2b97f75feebc804c4e9a15f3c5837e4daea5eba3
rho <rho.rhoit@gmail.com>
2014-10-02 17:50:50 +0545

    adding showterm screencast support
    * updated install and configure script
    * network proxy code refactoring
    * prompt file rename to generic
    * emacs switcher update

--- shell/install.sh
+++ shell/install.sh
@@ -3,14 +3,19 @@
 list="zshrc bashrc shellrc shellrc.d"
 
 for item in $list; do
-    echo $item
+    ( file ~/.$item | grep -v "symbolic" ) || continue
+    echo $item starting..
     if [[ -e ~/.$item ]]; then
-	name="$item$(date +%s).bak"
-	mv ~/.$item ~/$name
-	echo "Old config has been renamed as $name"
+	    name="$item$(date +%s).bak"
+	    mv ~/.$item ~/$name
+	    echo "Old config has been renamed as $name"
     fi
     ln -s $PWD/$item ~/.$item
 done
 
-cp tmp/git-prompt.sh /opt/
-chmod +x /opt/git-prompt.sh
+if [[ ! -e /tmp/git-prompt.sh && -e /usr/share/git/git-prompt.sh ]]; then
+    echo "Try running configure again"
+else
+    cp /tmp/git-prompt.sh /opt/
+    chmod +x /opt/git-prompt.sh
+fi
