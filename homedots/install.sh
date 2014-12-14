9b9abcb2fbb0c7c236605c6b8fe0d8cf84a272d4
rho <rho.rhoit@gmail.com>
2014-12-14 22:19:00 +0545

    check before install

--- homedots/install.sh
+++ homedots/install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-list="gmrunrc mg pythonrc tmux.conf xinitrc xlerc"
+PKGFILE=(gmrun mg python tmux X xle)
+INSTALL_LIST=(gmrunrc mg pythonrc tmux.conf xinitrc xlerc)
+
+for ((i=${#PKGFILE}; i >= 0; i--)); do
+    which ${PKGFILE[i]} || continue
+    list+="${INSTALL_LIST[i]} "
+done
 
 for item in $list; do
     ( file ~/.$item | grep -v "symbolic" ) || continue
+    which PKG
     echo $item starting..
     if [[ -e ~/.$item ]]; then
 	    name="$item$(date +%s).bak"
