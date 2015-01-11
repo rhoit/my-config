30882363224e1d96f7d1cd02fa5c7a81bab2f5e3
rho <rho.rhoit@gmail.com>
2015-01-12 00:20:05 +0545

    install method update
    * Makefile updated
    * readme added
    * refactoring

--- shell/install.sh
+++ shell/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-list="zshrc bashrc shellrc shellrc.d"
+if [[ "$1" == "" ]]; then
+    COMMAND="ln -s"
+else
+    COMMAND="cp"
+fi
+
+list="bashrc shellrc shellrc.d zshrc"
 
 for item in $list; do
     ( file ~/.$item | grep -v "symbolic" ) || continue
@@ -10,12 +16,5 @@ for item in $list; do
 	    mv ~/.$item ~/$name
 	    echo "Old config has been renamed as $name"
     fi
-    ln -s $PWD/$item ~/.$item
+    $COMMAND $PWD/$item ~/.$item
 done
-
-if [[ ! -e /tmp/git-prompt.sh && -e /usr/share/git/git-prompt.sh ]]; then
-    echo "Try running configure again"
-else
-    cp /tmp/git-prompt.sh /opt/
-    chmod +x /opt/git-prompt.sh
-fi
