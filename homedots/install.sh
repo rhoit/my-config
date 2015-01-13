b59ca2a297bc5b13732dd8a7eebcfdeffb43d067
rho <rho.rhoit@gmail.com>
2015-01-13 21:53:32 +0545

    installation Methods
    * updating README
    * update install methods

--- homedots/install.sh
+++ homedots/install.sh
@@ -1,21 +1,12 @@
 #!/bin/bash
 
-PKGFILE=(gmrun mg python tmux X xle)
-INSTALL_LIST=(gmrunrc mg pythonrc tmux.conf xinitrc xlerc)
-
-for ((i=${#PKGFILE}; i >= 0; i--)); do
-    which ${PKGFILE[i]} || continue
-    list+="${INSTALL_LIST[i]} "
-done
-
-for item in $list; do
-    ( file ~/.$item | grep -v "symbolic" ) || continue
-    which PKG
-    echo $item starting..
+rm -f installed
+while read item; do
+    echo "~/.$item" >> installed
     if [[ -e ~/.$item ]]; then
 	    name="$item$(date +%s).bak"
 	    mv ~/.$item ~/$name
 	    echo "Old config has been renamed as $name"
     fi
     ln -s $PWD/$item ~/.$item
-done
+done < conf
