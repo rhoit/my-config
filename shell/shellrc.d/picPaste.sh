7a25ced4331e91fbcd3e442a8c5d30cceaa84279
rho <rho.rhoit@gmail.com>
2016-02-09 14:51:33 +0545

    shell ~ rc.d/picPaste.sh picpaste-screen

--- shell/shellrc.d/picPaste.sh
+++ shell/shellrc.d/picPaste.sh
@@ -6,3 +6,9 @@ function picpaste {
     opts=( -F storetime=1 -F addprivacy=1 -F rules=yes )
     curl -sA firefox "${opts[@]}" -F upload=@"$1" "$link" | sed -n '/Picture URL/{n;s/.*">//;s/<.*//p}'
 }
+
+
+function picpaste-screen {
+    eog $(ls -t ~/*.png | head -1)
+    picpaste $(ls -t ~/*.png | head -1)
+}
