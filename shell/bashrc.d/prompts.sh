7ddfbbfdca3ddb8e53f1b905a28e9ca4ab4a760b
rho <rho.rhoit@gmail.com>
2016-02-09 13:12:16 +0545

    shell ~ bashrc.d/prompt.sh bye-bye ubuntu

--- shell/bashrc.d/prompts.sh
+++ shell/bashrc.d/prompts.sh
@@ -6,17 +6,8 @@ function prompt-def {
 
 
 function prompt-git {
-    git_path='/usr/share/git/git-prompt.sh'  # missing in ubuntu git package
-    if [[ ! -e $git_path  ]]; then
-        git_path="/opt/git-prompt.sh"
-    fi
-
-    if [[ -e  $git_path ]]; then
-        source $git_path
-        PS1='[\[\033[01;32m\]\u@\h\[\033[01;34m\] \W\[\e[0;32m\]$(__git_ps1 " (%s)")\[\033[00m\]]\$ '
-    else
-        prompt-def
-    fi
+    source '/usr/share/git/git-prompt.sh'  # missing in ubuntu git package
+    PS1='[\[\033[01;32m\]\u@\h\[\033[01;34m\] \W\[\e[0;32m\]$(__git_ps1 " (%s)")\[\033[00m\]]\$ '
 }
 
 
