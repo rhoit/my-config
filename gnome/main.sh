ebc8b14bc512fc123f23d0f28f47620f7fc79cf0
rho <rho.rhoit@gmail.com>
2016-02-15 16:03:35 +0545

    set gnome-terminal configs

--- gnome/main.sh
+++ gnome/main.sh
@@ -5,6 +5,24 @@
 # gsettings set org.gnome.desktop.default-applications.terminal exec-arg "'-e'"
 
 # pdf: evince
-cp ~/.local/share/applications/mimeapps.list .
+#cp ~/.local/share/applications/mimeapps.list .
 
+# * gnome-terminal configs
+function gnome-terminal-config {
+    # data are stored in ~/.gconf/apps/gnome-terminal
+    gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar "false"
+    gsettings set org.gnome.Terminal.Legacy.Settings mnemonics-enabled "false"
 
+    profile=$(dconf list /org/gnome/terminal/legacy/profiles:/)
+    if [[ "${profile:0:1}" != ":" ]]; then
+        echo "profile not found"
+        return
+    fi
+
+    dconf write /org/gnome/terminal/legacy/profiles:/${profile}background-color "'rgb(0,0,0)'"
+    dconf write /org/gnome/terminal/legacy/profiles:/${profile}use-theme-colors "false"
+    dconf write /org/gnome/terminal/legacy/profiles:/${profile}scrollback-lines "9999"
+    dconf write /org/gnome/terminal/legacy/profiles:/${profile}foreground-color "'rgb(255,255,255)'"
+}
+
+gnome-terminal-config
