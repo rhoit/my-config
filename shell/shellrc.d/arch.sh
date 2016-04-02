80ce19c5acfaaa8d50ada893659d1dae15bbbe9b
rho <rho.rhoit@gmail.com>
2016-04-02 22:40:59 +0545

    shell - arch.sh arch-outsource-pkg-download()

--- shell/shellrc.d/arch.sh
+++ shell/shellrc.d/arch.sh
@@ -3,6 +3,21 @@
 #
 ## application related to archlinux
 
+function arch-outsource-pkg-download {
+    (( $# == 0 )) && {
+        echo "Usage:"
+        echo "  $ arch-outsource-pkg-download user@ip /path/to/download/folder/"
+        return
+    }
+    pacman -Sup | grep http > /tmp/pkglst
+    scp /tmp/pkglst $1:$2
+    ssh $1 tmux send-keys "mkdir\ \-p\ $2/pkg/" Enter
+    ssh $1 tmux send-keys "cd\ $2/pkg" Enter
+    ssh $1 tmux send-keys 'while\ read\ i\;\ do' Enter
+    ssh $1 tmux send-keys 'wget\ \-c\ \$i' Enter
+    ssh $1 tmux send-keys 'done\< ../pkglst' Enter
+}
+
 
 function arch-build-iso {
     # first argument take old archlive directory
