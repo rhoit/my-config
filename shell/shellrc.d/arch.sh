5b65206534f3c5ecb5883ab41b60e48dbf4560ca
rho <rho.rhoit@gmail.com>
2016-04-02 22:40:59 +0545

    shell - arch package sync

--- shell/shellrc.d/arch.sh
+++ shell/shellrc.d/arch.sh
@@ -3,6 +3,33 @@
 #
 ## application related to archlinux
 
+function arch-outsource-pkg-download {
+    (( $# == 0 )) && {
+        echo "Usage:"
+        echo "  $ arch-outsource-pkg-download user@ip /path/to/download/folder/"
+        return
+    }
+    pacman -Sup | grep http > /tmp/pkglst
+    scp /tmp/pkglst $1:$2
+    ssh $1 tmux send-keys "mkdir\ \-p\ $2/pkg/" Enter
+    ssh $1 tmux send-keys "cd\ $2/pkg" Enter
+    ssh $1 tmux send-keys 'while\ read\ i\;\ do' Enter
+    ssh $1 tmux send-keys 'wget\ \-c\ \$i' Enter
+    ssh $1 tmux send-keys 'done\< ../pkglst' Enter
+}
+
+
+function arch-rsync-pkg-cache {
+    (( $# == 0 )) && {
+        echo "Usage:"
+        echo "  $ arch-rsync-pkg-cache user@ip [/path/to/cache/folder/] [/path/to/remote/cache/folder]"
+        return
+    }
+    pacman -Sup | grep http | xargs -n1 basename  > /tmp/pkglst
+    wc -l /tmp/pkglst
+    sudo rsync -r --append --progress --files-from=/tmp/pkglst $1:${2:=/var/cache/pacman/pkg} ${3:=/var/cache/pacman/pkg}
+}
+
 
 function arch-build-iso {
     # first argument take old archlive directory
