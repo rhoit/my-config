942fa286b355e88f978178aff0f18a2ffa1d794b
rho <rho.rhoit@gmail.com>
2016-04-20 22:35:41 +0545

    xfce4

---
+++ xfce4/thunar.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# thunar open-terminal shortcut
+# TODO dectect thunar is installed
+
+/home/username/.config/Thunar/uca.xml
+# (gtk_accel_path "<Actions>/ThunarActions/uca-action-1398672656481710-1" "<Alt>t")
+
+echo "to changes to appear restart thunar"
