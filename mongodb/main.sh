6436af61e1bf2eabca90c63101c8a663db1eca9c
rho <rho.rhoit@gmail.com>
2017-04-14 20:03:29 +0545

    mongo hacker without npm

--- mongodb/main.sh
+++ mongodb/main.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-# pacman -S npm # needs npm to be installed
-npm -g install mongo-hacker
+## without npm
+
+git clone --depth=1 https://github.com/TylerBrock/mongo-hacker /tmp/mongo-hacker
+cd /tmp/mongo-hacker
+sed -i 's/ln -sf/cp/' /tmp/mongo-hacker/Makefile
+make install
+
+## with npm
+# pacman -S npm
+# npm -g install mongo-hacker
 # cd /usr/lib/node_modules/mongo-hacker/
 # make install
