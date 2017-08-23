a5a87a40870c15bbcfc712479291ddb097947814
rho <rho.rhoit@gmail.com>
2017-08-23 22:18:42 +0545

    update gnome conf

--- gnome/main.sh
+++ gnome/main.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Sample
+# data are stored in ~/.gconf/apps/gnome-terminal
 # gsettings set org.gnome.desktop.default-applications.terminal exec konsole
 # gsettings set org.gnome.desktop.default-applications.terminal exec-arg "'-e'"
 
 # pdf: evince
 # cp ~/.local/share/applications/mimeapps.list .
 
-function gnome-terminal-conf {
-    # data are stored in ~/.gconf/apps/gnome-terminal
-    gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar "false"
-    gsettings set org.gnome.Terminal.Legacy.Settings mnemonics-enabled "false"
+function terminal-conf {
+    dconf write /org/gnome/terminal/legacy/theme-variant "'dark'" # dark-theme
+    dconf write /org/gnome/terminal/legacy/default-show-menubar "false"
+    dconf write /org/gnome/terminal/legacy/mnemonics-enabled "false"
 
     local PROFILE=($(dconf list /org/gnome/terminal/legacy/profiles:/ | grep '^:'))
     if (( "${#PROFILE}" == 0 )); then
@@ -25,22 +26,29 @@ function gnome-terminal-conf {
 }
 
 
-function gnome-privacy-conf {
+function privacy-conf {
     dconf write /org/gnome/desktop/privacy/remember-recent-files "false"
     dconf write /org/gnome/desktop/privacy/send-software-usage-stats "false"
     dconf write /org/gnome/desktop/privacy/recent-files-max-age "0"
     dconf write /org/gnome/system/location/enabled "false"
-    rm ~/.local/share/recently-used.xbel
+    rm -f ~/.local/share/recently-used.xbel
 }
 
 
-function gnome-desktop-conf {
+function desktop-conf {
     dconf write /org/gnome/desktop/background/show-desktop-icons "false"
 }
 
 
+function nautilus-conf {
+    dconf write /org/gnome/nautilus/preferences/recursive-search "'never'"
+    dconf write /org/gnome/nautilus/preferences/show-image-thumbnails "'never'"
+}
+
+
 if [[ $(basename "$0") == "main.sh" ]]; then
-    gnome-terminal-conf
-    gnome-privary-conf
-    gnome-desktop-conf
+    terminal-conf
+    privacy-conf
+    desktop-conf
+    nautilus-conf
 fi
