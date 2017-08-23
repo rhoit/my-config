af4cdc428f1246035ce6121aafaada920feb2ffc
rho <rho.rhoit@gmail.com>
2017-08-23 19:57:17 +0545

    git - main.sh github

--- git/main.sh
+++ git/main.sh
@@ -8,6 +8,9 @@ set -e
 
 git config --global core.editor mg
 git config --global color.ui true
+git config --global color.diff.old "red bold"
+git config --global color.diff.new "green italic"
+
 git config --global push.default simple # git 2.0 fix warnings
 
 
@@ -28,6 +31,15 @@ git config --global user.name "${uname:-$d_UNAME}"
 git config --global user.email "${email:-$d_EMAIL}"
 
 
+# * GITHUB CONFIG
+
+read -r -p "Do you want to config for github (y/n):" r
+if [[ "$r" == "" || "$r" == "Y" || "$r" == 'y' ]]; then
+    true
+else
+    exit
+fi
+
 d_GITHUB=$(git config --get github.user)
 echo '* Github gist Setup'
 read -p "    github-username: $d_GITHUB" hub_user
@@ -36,19 +48,3 @@ read -sp "    github-password: " hub_pass; echo
 curl -u "${hub_user:=$d_GITHUB}:${hub_pass}"\
      -X GET /authorizations\
      https://api.github.com/authorizations
-
-exit
-
-curl -u "${hub_user:=$d_hub_user}:${hub_pass}"\
-     -H "Content-Type: application/json"\
-     -X POST\
-     -d '{"scopes":["gist"], "note": "gist"}'\
-     https://api.github.com/authorizations > /tmp/gitapi
-
-cat /tmp/gitapi
-
-token=$(sed -n '/"token"/s/.*: "\(.*\)",/\1/p' /tmp/gitapi)
-echo $token
-
-git config --global github.user "$hub_user"
-git config --global github.gist-oauth-token "$token"
