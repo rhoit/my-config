38ab3614c940ee2fb47638711136472a500b2057
rho <rho.rhoit@gmail.com>
2017-08-23 19:44:47 +0545

    gnome configs

--- gnome/main.sh
+++ gnome/main.sh
@@ -5,24 +5,42 @@
 # gsettings set org.gnome.desktop.default-applications.terminal exec-arg "'-e'"
 
 # pdf: evince
-#cp ~/.local/share/applications/mimeapps.list .
+# cp ~/.local/share/applications/mimeapps.list .
 
-# * gnome-terminal configs
-function gnome-terminal-config {
+function gnome-terminal-conf {
     # data are stored in ~/.gconf/apps/gnome-terminal
     gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar "false"
     gsettings set org.gnome.Terminal.Legacy.Settings mnemonics-enabled "false"
 
-    profile=$(dconf list /org/gnome/terminal/legacy/profiles:/)
-    if [[ "${profile:0:1}" != ":" ]]; then
-        echo "profile not found"
+    local PROFILE=($(dconf list /org/gnome/terminal/legacy/profiles:/ | grep '^:'))
+    if (( "${#PROFILE}" == 0 )); then
+        echo "no profiles found"
         return
     fi
 
-    dconf write /org/gnome/terminal/legacy/profiles:/${profile}background-color "'rgb(0,0,0)'"
-    dconf write /org/gnome/terminal/legacy/profiles:/${profile}use-theme-colors "false"
-    dconf write /org/gnome/terminal/legacy/profiles:/${profile}scrollback-lines "9999"
-    dconf write /org/gnome/terminal/legacy/profiles:/${profile}foreground-color "'rgb(255,255,255)'"
+    echo "configuring for ${PROFILE[0]}"
+    dconf write /org/gnome/terminal/legacy/profiles:/${PROFILE}background-color "'rgb(0,0,0)'"
+    dconf write /org/gnome/terminal/legacy/profiles:/${PROFILE}use-theme-colors "false"
+    dconf write /org/gnome/terminal/legacy/profiles:/${PROFILE}foreground-color "'rgb(255,255,255)'"
 }
 
-gnome-terminal-config
+
+function gnome-privacy-conf {
+    dconf write /org/gnome/desktop/privacy/remember-recent-files "false"
+    dconf write /org/gnome/desktop/privacy/send-software-usage-stats "false"
+    dconf write /org/gnome/desktop/privacy/recent-files-max-age "0"
+    dconf write /org/gnome/system/location/enabled "false"
+    rm ~/.local/share/recently-used.xbel
+}
+
+
+function gnome-desktop-conf {
+    dconf write /org/gnome/desktop/background/show-desktop-icons "false"
+}
+
+
+if [[ $(basename "$0") == "main.sh" ]]; then
+    gnome-terminal-conf
+    gnome-privary-conf
+    gnome-desktop-conf
+fi
