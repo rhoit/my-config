9719ede4746303e00aa3107da8d8e4b2be9b9944
rho <rho.rhoit@gmail.com>
2018-03-03 18:47:02 +0545

    shell - youtube-dl download interactive

--- shell/shellrc.d/rho.sh
+++ shell/shellrc.d/rho.sh
@@ -190,3 +190,27 @@ function dlna {
 
     sudo minidlnad -R -d -f /tmp/dlna.conf
 }
+
+# ** youtube-dl
+
+function ydl {
+    local list=$(youtube-dl --list-formats $1)
+
+    echo $list | sed -n '/[0-9]x[0-9]/p'
+    echo -n "video format (default=244, skip=0): "; read video
+    if [[ "$video" == 0 ]]; then
+        video=""
+    else
+        video="${video:-244}+"
+    fi
+
+    echo $list | sed -n '/audio only/p'
+    echo -n "audio format (default=250, skip=0): "; read audio
+    if [[ "$audio" == 0 ]]; then
+        audio=""
+    else
+        audio="${audio:-250}"
+    fi
+
+    youtube-dl --format "${video}${audio}" $1
+}
