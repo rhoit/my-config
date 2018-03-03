e977ad2d262bee351b66b55fdd22b839e8b0d299
rho <rho.rhoit@gmail.com>
2018-03-03 18:47:02 +0545

    shell - shellrc.d/rho.sh ydl()

--- shell/shellrc.d/rho.sh
+++ shell/shellrc.d/rho.sh
@@ -82,3 +82,27 @@ alias woman="man --apropos $1"
 
 # ** wacom
 alias wacom-touch-off='xsetwacom set "Wacom Bamboo 16FG 4x5 Finger pad" touch off'
+
+
+# ** youtube-dl
+function ydl {
+    local list=$(youtube-dl --list-formats $1)
+
+    echo $list | sed -n '/[0-9]x[0-9]/p'
+    echo -n "video format (default=244, skip=0): "; read video
+    if [[ "$video" == 0 ]]; then
+        video=""
+    else
+        video="${video:-244}+"
+    fi
+
+    echo $list | sed -n '/audio only/p'
+    echo -n "audio format (default=250, skip=0): "; read audio
+    if [[ "$audio" == 0 ]]; then
+        audio=""
+    else
+        audio="${audio:-250}"
+    fi
+
+    youtube-dl --format "${video}${audio}" $1
+}
