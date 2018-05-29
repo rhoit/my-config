b13ebe0ea152aeac26a4b1c5a0c1e38f2dcb28c0
rho <rho.rhoit@gmail.com>
2018-05-29 19:27:07 +0545

    docker - more clean up

--- shell/shellrc.d/docker.sh
+++ shell/shellrc.d/docker.sh
@@ -4,16 +4,17 @@ function docker_fit {
     # docker fit output
     tput rmam # line-wrap off
     docker $@ | sed '
-        1s/ *NAMES$//g;          # for ps
-        s/ *[a-z]\+_[a-z]\+$//g;
-        s/"\(.*\)"/\1/g;         # for ps
+        1s/ *NAMES$//g;          # remove NAMES from ps
+        s/ *[a-z]\+_[a-z]\+$//g; # remove NAMES from ps
+
+        s/"\(.*\)"/\1/g;         # remove quotes from ps
         s/ seconds\?/s/g;
         s/ minutes\?/m/g;
         s/ hours\?/h/g;
         s/ days\?/d/g;
         s/ weeks\?/w/g;
         s/ months\?/m/g;
-        s/About an\?/1m/g;
+        s/About an\?/1/g;
         s/Exited (\([0-9]\+\)) \(.*\)ago/exit(\1)~\2/; # for ps
         s/->/→/g
         ' |
@@ -46,20 +47,28 @@ function dlog {
 }
 
 
+function dins {
+    docker inspect $1 | jq
+}
+
+
+
 alias dpull='docker pull'
 alias dinfo='docker info'
-alias dst='docker status'
 alias drun='docker run'
 alias drace='docker run --rm'
 alias dstop='docker stop'
 alias dexec='docker exec'
 alias dl='docker ps --latest --quiet'
 alias dtty='docker run --interactive --tty'
-alias dins='docker inspect'
-alias dlins="docker inspect $(dl) | jq"
+
+# alias dlins="docker inspect $(dl) | jq"
+
+
 
 alias dc='docker-compose'
 
+alias dtop='docker_fit top'
 alias dimg='docker_fit images'
 alias dll='docker_fit ps --latest'
 alias dps='docker_fit ps --all'
