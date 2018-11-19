66a5de08490d87778d6e36ad9a4cc39a752ecdac
rho <rho.rhoit@gmail.com>
2018-11-19 07:30:55 +0545

    dosbox - updated version

--- dosbox/main.sh
+++ dosbox/main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Changing keyboardlayout to dvorak (dv103)"
-sed -i 's/keyboardlayout=auto/keyboardlayout=dv103/' ~/.dosbox/dosbox-0.74.conf
+sed -i 's/keyboardlayout=auto/keyboardlayout=dv103/' ~/.dosbox/dosbox-0.74-2.conf
