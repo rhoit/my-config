503847bc5c2fe2065dd0de95c574ceaa997ce5bb
rho <rho.rhoit@gmail.com>
2018-11-19 07:38:32 +0545

    shell - refactor and shuffle

--- shell/shellrc.d/bashTricks.sh
+++ shell/shellrc.d/bashTricks.sh
@@ -4,15 +4,17 @@
 # Bash tricks
 # handy shortcuts for basic commands and enable color
 
+# grep
+alias grep='grep --color=auto '
+alias ggrep='grep --exclude-dir={.git} '
+
 # ls
 alias l='ls -CFh'
 alias ll='ls -lFh'
 alias la='ls -AFh'
 alias lla='ls -aFlh'
-
-# grep
-alias grep='grep --color=auto '
-alias ggrep='grep --exclude-dir={.git} '
+alias lgrep='ls | grep '
+alias lagrep='ls -a | grep '
 
 # cd
 alias ..='cd ..'
@@ -32,17 +34,12 @@ alias sudo='sudo -E '
 # she know all (kinda from emacs)
 alias woman="man --apropos $1"
 
+# history
+alias hist='history | grep '
 
 
-#----------------------------------------------------------------------
-# COMPOSITE ALIAS
 # lazy conf
 function run { chmod +x "$@" && ./$1; }
 function mkcd { mkdir -p "$1" && cd "$1"; }
 
-# ls | grep
-alias lgrep='ls | grep '
-alias lagrep='ls -a | grep '
 
-# history
-alias hist='history | grep '
