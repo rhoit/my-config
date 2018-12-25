b4c8320fb11f98cf435fc245f2a2f72db4625bd1
rho <rho.rhoit@gmail.com>
2018-12-26 05:27:35 +0545

    shell - adding commentry

--- shell/shellrc.d/xterm.sh
+++ shell/shellrc.d/xterm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 function xterm-zsh-keybind-hack {
+    # open new xterm on same location
+    # simalar to C-S-n in gnome and xfce terminals
     bindkey -s '^N' 'setsid xterm^M'
 }
 
