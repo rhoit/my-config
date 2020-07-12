00a0cc0b1938898c0f6464077072b257a0e1c1a5
rho <rho.rhoit@gmail.com>
2020-07-12 20:50:20 +0545

    docker - privileged systemd container

---
+++ docker/systemd/sysd.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# Based on
+## https://developers.redhat.com/blog/2014/05/05/running-systemd-within-docker-container/
+
+pacman --noconfirm --needed --sync systemd systemd-sysvcompat
+rm -f /etc/systemd/system/*.wants/*
+(
+    cd /lib/systemd/system/sysinit.target.wants/;
+    for i in *; do
+        [ $i == systemd-tmpfiles-setup.service ] && continue
+        rm -f $i
+    done
+)
+
+rm -f /lib/systemd/system/basic.target.wants/*
+rm -f /lib/systemd/system/local-fs.target.wants/*
+rm -f /lib/systemd/system/multi-user.target.wants/*
+rm -f /lib/systemd/system/sockets.target.wants/*initctl*
+rm -f /lib/systemd/system/sockets.target.wants/*udev*
+
+pacman --noconfirm --needed --sync openssh
+systemctl enable sshd.service
+mkdir -p /root/.ssh/
+cp /tmp/id_rsa.pub /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
