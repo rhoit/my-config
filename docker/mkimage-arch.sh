43b858e32c479103a6da84648d5e9bf77784ebde
rho <rho.rhoit@gmail.com>
2020-07-18 10:24:09 +0545

    docker - locale fix ordering

--- docker/mkimage-arch.sh
+++ docker/mkimage-arch.sh
@@ -28,8 +28,6 @@ if [[ -d $ROOTFS ]]; then
 fi
 
 umask 022 # reset umask to default
-export LANG="C.UTF-8"
-export LC_ALL="C"
 
 # bootstrap
 mkdir -pv "${ROOTFS}/var/lib/pacman/sync"
@@ -57,12 +55,22 @@ Include = /etc/pacman.d/mirrorlist
 Include = /etc/pacman.d/mirrorlist
 
 [options]
+NoExtract = etc/systemd/*
+NoExtract = lib/systemd/*
+NoExtract = usr/share/X11/locale/*
+NoExtract = usr/share/common-lisp/*
+NoExtract = usr/share/doc/*
+NoExtract = usr/share/fish/*
+NoExtract = usr/share/gdb/*
+NoExtract = usr/share/help/*
+NoExtract = usr/share/i18n/charmaps/* !usr/share/i18n/charmaps/UTF-8.gz
 NoExtract = !usr/share/i18n/locales/en_GB
 NoExtract = !usr/share/i18n/locales/en_US
 NoExtract = !usr/share/i18n/locales/i18n
 NoExtract = !usr/share/i18n/locales/i18n_ctype
 NoExtract = !usr/share/i18n/locales/iso14651_t1
 NoExtract = !usr/share/i18n/locales/iso14651_t1_common
+NoExtract = !usr/share/i18n/locales/locale.alias
 NoExtract = !usr/share/i18n/locales/translit_circle
 NoExtract = !usr/share/i18n/locales/translit_cjk_compat
 NoExtract = !usr/share/i18n/locales/translit_combining
@@ -73,19 +81,9 @@ NoExtract = !usr/share/i18n/locales/translit_narrow
 NoExtract = !usr/share/i18n/locales/translit_neutral
 NoExtract = !usr/share/i18n/locales/translit_small
 NoExtract = !usr/share/i18n/locales/translit_wide
-NoExtract = etc/systemd/*
-NoExtract = lib/systemd/*
-NoExtract = usr/share/X11/locale/*
-NoExtract = usr/share/common-lisp/*
-NoExtract = usr/share/doc/*
-NoExtract = usr/share/fish/*
-NoExtract = usr/share/gdb/*
-NoExtract = usr/share/help/*
-NoExtract = usr/share/i18n/charmaps/* !usr/share/i18n/charmaps/UTF-8.gz
-NoExtract = usr/share/i18n/locales/*
 NoExtract = usr/share/info/*
 NoExtract = usr/share/licenses/*
-NoExtract = usr/share/locale/*
+NoExtract = usr/share/locale/* !usr/share/locale/locale.alias
 NoExtract = usr/share/man/*
 NoExtract = usr/share/vala/*
 NoExtract = usr/share/vim/*
@@ -108,6 +106,7 @@ arch-chroot $ROOTFS /bin/sh -c "pacman-key --init"
 arch-chroot $ROOTFS /bin/sh -c "pacman-key --populate archlinux"
 
 # arch-chroot $ROOTFS /bin/sh -c "ln -s /usr/share/zoneinfo/UTC /etc/localtime"
+echo 'LANG=en_US.UTF-8' > $ROOTFS/etc/locale.conf
 echo 'en_US.UTF-8 UTF-8' > $ROOTFS/etc/locale.gen
 arch-chroot $ROOTFS locale-gen
 
