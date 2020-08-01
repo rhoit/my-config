919174e466f98e304807c42a53cd62fde13b0f57
rho <rho.rhoit@gmail.com>
2020-08-02 00:21:19 +0545

    docker - psql prompt and server

--- docker/pg/docker-entrypoint.sh
+++ docker/pg/docker-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/bash
 
 initdb --username='postgres' --locale en_US.UTF-8 --pgdata /var/lib/postgres/data
-PGUSER=postgres pg_ctl --wait --pgdata=/var/lib/postgres/data start
-psql
+
+if [[ "$1" == psql ]]; then
+    pg_ctl --wait --pgdata=/var/lib/postgres/data start
+    psql
+else
+    echo "host all all all trust" >> "/var/lib/postgres/data/pg_hba.conf"
+    postgres -h '*' -D /var/lib/postgres/data
+fi
