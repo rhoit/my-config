279c1cd8eceb811645a934e17b952e08fe21a14e
rho <rho.rhoit@gmail.com>
2021-05-01 12:49:19 +0545

    git - refactor config

--- git/main.sh
+++ git/main.sh
@@ -10,8 +10,18 @@ git config --global core.editor mg
 git config --global color.ui true
 git config --global color.diff.old "red bold"
 git config --global color.diff.new "green italic"
+git config --global diff.colormoved default
 
 git config --global push.default simple # git 2.0 fix warnings
+git config --global pull.rebase true
+
+# git config --global core.whitespace trailing-space,-space-before-tab,indent-with-non-tab,tab-in-indent,cr-at-eol
+# git config --global core.whitespace -space-before-tab,indent-with-non-tab,tab-in-indent,cr-at-eol
+
+# https://www.git-scm.com/book/en/v2/Git-Tools-Signing-Your-Work#_signing
+# git config --global user.signingkey <gpg-key-id>
+
+git config --global core.attributesfile "${PWD}/gitattributes"
 
 
 # * GIT USER SETUP
