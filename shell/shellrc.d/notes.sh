ba6702d0682c0a55c6e9ec684d4a6c3c047b98f8
rho <rho.rhoit@gmail.com>
2021-05-20 08:47:02 +0545

    shell - notes.sh ffmpeg

---
+++ shell/shellrc.d/notes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#
+# notes
+# nifty thing cheatsheet
+
+
+function notes-ffmpeg {
+    echo "* probe file"
+    echo ffprobe video.webm
+    echo
+
+    echo "* entract audio"
+    echo ffmpeg -i video.mkv -map 0:a -acodec copy audio.mp4
+    echo ffmpeg -i video.mkv -map 0:a -acodec libmp3lame audio.mp3
+    echo
+
+    echo "* re-encode"
+    echo ffmpeg -i input.file -codec:a aac -codec:v libx264  output.mp4
+    echo
+
+    echo "* png to webm"
+    echo ffmpeg -i input%04d.png output.webm
+    echo
+}
