decd18a6fba787e24cc3fe0810dbc53348a40653
rho <rho.rhoit@gmail.com>
2023-05-28 14:35:38 +0545

    nemo - gsettings

---
+++ nemo/main.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+
+# for gui use dconf-editor
+
+gsettings set org.nemo.desktop show-desktop-icons false
+gsettings set org.cinnamon.desktop.default-applications.terminal exec xfce4-terminal
