3f3f8d041af9f70e95abb5a16b222961527c7366
rho <rho.rhoit@gmail.com>
2023-05-28 14:32:16 +0545

    bin - override for chromium

--- bin/override.sh
+++ bin/override.sh
@@ -4,6 +4,13 @@ set -e
 set -v
 
 
+FILE=/usr/local/bin/chromium
+cat > $FILE <<EOF
+exec /usr/bin/chromium --incognito --allow-file-access-from-file "\$@"
+EOF
+chmod +x $FILE
+
+
 FILE=/usr/local/bin/gitg
 cat > $FILE <<EOF
 GTK_THEME=Adwaita:light exec /usr/bin/gitg "\$@"
