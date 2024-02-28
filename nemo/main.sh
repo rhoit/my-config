69d81027997da6ed2b74817d3d77e367d764d005
rho <rho.rhoit@gmail.com>
2024-02-28 08:49:11 +0545

    nemo - gsettings show-location-entry

--- nemo/main.sh
+++ nemo/main.sh
@@ -4,4 +4,5 @@
 # for gui use dconf-editor
 
 gsettings set org.nemo.desktop show-desktop-icons false
+gsettings set org.nemo.preference.show-location-entry false  # show bread-crumb
 gsettings set org.cinnamon.desktop.default-applications.terminal exec xfce4-terminal
