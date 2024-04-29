6d90a4aaa436b8b8c4bfda4a22b2aed9f4aa2553
rho <rho.rhoit@gmail.com>
2024-04-29 15:09:53 +0545

    xdg - mime/install.sh

---
+++ Xdg/mime/install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+# might be the local path not tested
+# cp ~/.local/share/applications/mimeapps.list .
+
+xdg-mime install org-mode.xml
+xdg-mime install djhtml-mode.xml
+xdg-mime install scons-conf.xml
+
+# * set default
+xdg-mime default evince.desktop application/pdf
+
+# xdg-icon-resource install --context mimetypes --size 64 shiny-file-icon.png text-x-shiny
+
+# update mime database
+# echo update-mime-database /usr/share/mime
